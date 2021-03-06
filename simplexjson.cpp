ebefc276c740adc12dce3e0ab87ce1d97ce2e296
imhdx <mhdxacmer@126.com>
2021-03-06 13:07:59 +0800

    完成主体代码

---
+++ simplexjson.cpp
@@ -0,0 +1,296 @@
+#include "simplexjson.h"
+#include <cassert>
+#include <error.h>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+
+#define EXCEPT(c,ch) do{assert(*c->json==(ch));c->json++;}while(0)
+#define ISDIGIT(CH) ((CH)>='0'&&(CH)<='9')
+#define ISDIGIT1TO9(ch) ((ch)>='1'&&(ch)<='9')
+
+struct simplex_context{
+  const char *json;
+};
+
+// 没有错误返回，因为whitespace可以为空，所以一定正确
+static void simplex_parse_whitespace(simplex_context *c){
+  const char *p=c->json;
+  while (*p==' ' || *p=='\t' || *p=='\n' || *p=='\r')
+    p++;
+  c->json=p;
+}
+
+static int simplex_parse_literal(simplex_context *c, simplex_value *v, const char *literal, simplex_type type){
+  EXCEPT(c,literal[0]);
+  size_t i=0;
+  for (i=0;literal[i+1];i++){
+    if (c->json[i]!=literal[i+1])
+      return SIMPLEX_ERROR;
+  }
+  c->json += i;
+  v->type=type;
+  return SIMPLEX_PARSE_OK;
+}
+
+static int simplex_parse_number(simplex_context *c, simplex_value *v){
+  const char *p=c->json;
+  if (*p=='-') p++;
+  if (*p=='0') p++;
+  else{
+    if (!ISDIGIT1TO9(*p)) return SIMPLEX_ERROR;
+    for (p++;ISDIGIT(*p);p++);
+  }
+  if (*p=='.'){
+    p++;
+    if (!ISDIGIT(*p)) return SIMPLEX_ERROR;
+    for (p++;ISDIGIT(*p);p++);
+  }
+  if (*p=='e' || *p=='E'){
+    p++;
+    if (*p=='+'||*p=='-') p++;
+    if (!ISDIGIT(*p)) return SIMPLEX_ERROR;
+    for (p++;ISDIGIT(*p);p++);
+  }
+  // 为了后面正确判断errno
+  errno=0;
+  v->n=strtod(c->json,NULL);
+  if (errno==ERANGE && (v->n==HUGE_VAL || v->n==-HUGE_VAL))
+    return SIMPLEX_ERROR;
+  v->type=SIMPLEX_NUMBER;
+  c->json=p;
+  return SIMPLEX_PARSE_OK;
+}
+
+static int simplex_parse_string(simplex_context *c, simplex_value *v){
+  const char *p;
+  EXCEPT(c,'\"');
+  p=c->json;
+  std::string tmp="";
+  while (true){
+    char ch=*p++;
+	switch (ch) {
+	  case '\"':
+	    simplex_set_string(v, tmp);
+	    c->json=p;
+	    return SIMPLEX_PARSE_OK;
+	  case '\\':
+		switch (*p++) {
+		  case '\"' : tmp+='\"';break;
+		  case '\\' : tmp+='\\';break;
+		  case '/' : tmp+='/';break;
+		  case 'b': tmp+='\b';break;
+		  case 'n' : tmp+='\n';break;
+		  case 'f' : tmp+='\f';break;
+		  case 'r' : tmp+='\r';break;
+		  case 't' : tmp+='\t';break;
+		  default:
+		    return SIMPLEX_ERROR;
+		}
+		break;
+	  case '\0':
+	    return SIMPLEX_ERROR;
+	  default:
+	    tmp+=ch;
+	}
+  }
+}
+
+static int simplex_parse_value(simplex_context *c, simplex_value *v);
+static int simplex_parse_array(simplex_context *c, simplex_value *v){
+  EXCEPT(c,'[');
+  simplex_parse_whitespace(c);
+  if (*c->json==']'){
+    // 空数组
+    c->json++;
+    v->type=SIMPLEX_ARRAY;
+    v->a.resize(0);
+    return SIMPLEX_PARSE_OK;
+  }
+  int ret;
+  while (true){
+    simplex_value e;
+    simplex_free(&e);
+    if ((ret=simplex_parse_value(c,&e))!=SIMPLEX_PARSE_OK)
+      break;
+    v->a.push_back(e);
+    simplex_parse_whitespace(c);
+    if (*c->json==','){
+      c->json++;
+      simplex_parse_whitespace(c);
+    }
+    else if (*c->json==']'){
+      c->json++;
+      v->type=SIMPLEX_ARRAY;
+      return SIMPLEX_PARSE_OK;
+    }
+    else{
+      ret=SIMPLEX_ERROR;
+      break;
+    }
+  }
+  return ret;
+}
+
+#include <unordered_map>
+typedef std::unordered_map<std::string,simplex_value*> object;
+
+static int simplex_parse_object(simplex_context *c, simplex_value *v){
+  EXCEPT(c, '{');
+  simplex_parse_whitespace(c);
+  if (*c->json=='}'){
+    c->json++;
+    v->type=SIMPLEX_OBJECT;
+    v->o={};
+    return SIMPLEX_PARSE_OK;
+  }
+  int ret;
+  object tmp;
+  while (true){
+    if (*c->json!='\"'){
+      ret=SIMPLEX_ERROR;
+      break;
+    }
+    simplex_value key;
+    simplex_value *val;
+    if ((ret=simplex_parse_string(c,&key))!=SIMPLEX_PARSE_OK){
+      break;
+    }
+    simplex_parse_whitespace(c);
+    if (*c->json!=':'){
+      ret=SIMPLEX_ERROR;
+      break;
+    }
+    else c->json++;
+    simplex_parse_whitespace(c);
+    val=new simplex_value;
+    if ((ret=simplex_parse_value(c,val))!=SIMPLEX_PARSE_OK){
+      delete val;
+      break;
+    }
+    printf("debug here\n");
+    tmp.insert({key.s,val});
+    simplex_parse_whitespace(c);
+    if (*c->json==','){
+      c->json++;
+      simplex_parse_whitespace(c);
+    }
+    else if (*c->json=='}'){
+      c->json++;
+      v->o=tmp;
+      v->type=SIMPLEX_OBJECT;
+      return SIMPLEX_PARSE_OK;
+    }
+    else{
+      ret=SIMPLEX_ERROR;
+      break;
+    }
+  }
+
+  // 发生了错误从这里走
+  for (auto &it:tmp){
+    delete it.second;
+  }
+  return ret;
+}
+
+static int simplex_parse_value(simplex_context *c, simplex_value *v){
+  switch (*c->json) {
+    case 't' : return simplex_parse_literal(c, v, "true", SIMPLEX_TRUE);
+    case 'f' : return simplex_parse_literal(c,v,"false",SIMPLEX_FALSE);
+    case 'n' : return simplex_parse_literal(c,v,"null", SIMPLEX_NULL);
+    case '\"': return simplex_parse_string(c,v);
+    case '\0': return SIMPLEX_ERROR; // 不允许空串
+    case '[' : return simplex_parse_array(c, v);
+    case '{' : return simplex_parse_object(c,v);
+	default  : return simplex_parse_number(c,v);
+  }
+}
+
+int simplex_parse(simplex_value* v,const char *json){
+  simplex_context c{};
+  int ret;
+  assert(v!=nullptr);
+  c.json=json;
+  simplex_free(v);
+  simplex_parse_whitespace(&c);
+  if ((ret=simplex_parse_value(&c,v))==SIMPLEX_PARSE_OK){
+    simplex_parse_whitespace(&c);
+    if (*c.json!='\0'){
+      v->type=SIMPLEX_NULL;
+      ret=SIMPLEX_ERROR;
+    }
+  }
+  return ret;
+}
+
+void simplex_free(simplex_value *v){
+  assert(v!=NULL);
+  v->s="";
+  v->a.clear();
+  for (auto &it:v->o){
+    delete it.second;
+  }
+  v->o.clear();
+  v->type=SIMPLEX_NULL;
+}
+
+simplex_type simplex_get_type(const simplex_value *v){
+  assert(v!=NULL);
+  return v->type;
+}
+
+int simplex_get_boolean(const simplex_value *v){
+  assert(v!=NULL && (v->type==SIMPLEX_TRUE || v->type==SIMPLEX_FALSE));
+  return v->type==SIMPLEX_TRUE;
+}
+
+void simplex_set_boolean(simplex_value *v, bool b){
+  simplex_free(v);
+  if (b) v->type=SIMPLEX_TRUE;
+  else v->type=SIMPLEX_FALSE;
+}
+
+double simplex_get_number(const simplex_value *v){
+  assert(v!=NULL&&v->type==SIMPLEX_NUMBER);
+  return v->n;
+}
+
+void simplex_set_number(simplex_value *v, double n){
+  simplex_free(v);
+  v->n=n;
+  v->type=SIMPLEX_NUMBER;
+}
+
+const std::string& simplex_get_string(const simplex_value *v){
+  assert(v!=NULL&&v->type==SIMPLEX_STRING);
+  return v->s;
+}
+
+size_t simplex_get_string_length(const simplex_value *v){
+  assert(v!=NULL && v->type==SIMPLEX_STRING);
+  return v->s.size();
+}
+
+void simplex_set_string(simplex_value *v,const std::string &s){
+  assert(v!=NULL);
+  simplex_free(v);
+  v->type=SIMPLEX_STRING;
+  v->s=s;
+}
+
+size_t simplex_get_array_size(const simplex_value *v){
+  assert(v!=NULL && v->type==SIMPLEX_ARRAY);
+  return v->a.size();
+}
+
+simplex_value simplex_get_array_element(const simplex_value *v, size_t index){
+  assert(v!=NULL && v->type==SIMPLEX_ARRAY);
+  assert(index<v->a.size());
+  return v->a[index];
+}
+
+size_t simplex_get_object_size(const simplex_value *v){
+  assert(v!=NULL && v->type==SIMPLEX_OBJECT);
+  return v->o.size();
+}
---
+++ simplexjson.h
@@ -0,0 +1,63 @@
+#ifndef __SIMPLEX_JSON
+#define __SIMPLEX_JSON
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+typedef enum{
+    SIMPLEX_NULL,
+    SIMPLEX_FALSE,
+    SIMPLEX_TRUE,
+    SIMPLEX_NUMBER,
+    SIMPLEX_ARRAY,
+    SIMPLEX_OBJECT,
+    SIMPLEX_STRING
+}simplex_type;
+
+
+struct simplex_value{
+  std::string s;
+  std::vector<simplex_value> a;
+  double n;
+  // 注意此处value用的指针
+  std::unordered_map<std::string, simplex_value*> o;
+  simplex_type type;
+};
+
+// 简单的errno
+// TODO
+enum {
+  SIMPLEX_PARSE_OK=0,
+  SIMPLEX_ERROR
+};
+//
+//void simplex_init(simplex_value *v){
+//  v->type=SIMPLEX_NULL;
+//}
+
+int simplex_parse(simplex_value *v, const char *json);
+
+void simplex_free(simplex_value *v);
+
+simplex_type simplex_get_type(const simplex_value* v);
+
+
+
+int simplex_get_boolean(const simplex_value *v);
+void simplex_set_boolean(simplex_value *v,int b);
+
+double simplex_get_number(const simplex_value *v);
+void simplex_set_number(simplex_value *v, double n0);
+
+const std::string& simplex_get_string(const simplex_value *v);
+size_t simplex_get_string_length(const simplex_value *v);
+void simplex_set_string(simplex_value *v,const std::string &s);
+
+size_t simplex_get_arrat_size(const simplex_value *v);
+simplex_value simplex_get_array_element(const simplex_value *v,size_t index);
+
+size_t simplex_get_object_size(const simplex_value *v);
+// const std::string& simplex_get_object_key(const simplex_value *v,size_t index);
+// size_t simplex_get_obejct_key_length(const simplex_value *v,size_t index);
+// simplex_value* simplex_get_object_value(const simplex_value *v, size_t index);
+#endif
